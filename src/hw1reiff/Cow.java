e102746566dd798788458412c0ab62edf06c2048
mreiff <mreiff@L15203039988.wctc.msft>
2014-09-02 12:57:07 -0500

    Last but not least, Cow now has the 4 methods overriden to give results as well.

--- src/hw1reiff/Cow.java
+++ src/hw1reiff/Cow.java
@@ -10,6 +10,29 @@ package hw1reiff;
  *
  * @author mreiff
  */
-public class Cow {
+public class Cow implements Animals{
     
+    @Override
+    public void breed(Animals animal) {
+        if(animal instanceof Cow){
+            System.out.println("There is 1 new cow");
+        }else{
+            System.out.println("No new Cows");
+        }
+    }
+
+    @Override
+    public void food() {
+        System.out.println("Steak");
+    }
+
+    @Override
+    public void interact(Mob mob) {
+        System.out.println("Passive");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Moo");
+    }
 }
