03393a08c0fb8a3b216cd168cc4f71e6cba6a815
mreiff <mreiff@L15203039988.wctc.msft>
2014-09-02 12:58:39 -0500

    The main method now has code that takes the methods from the other classes and uses them to make a small very poorly punctuated story.

--- src/hw1reiff/Hw1Reiff.java
+++ src/hw1reiff/Hw1Reiff.java
@@ -17,6 +17,22 @@ public class Hw1Reiff {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Mob zombie = new Zombie();
+        Creeper creeper = new Creeper();
+        Animals chicken1 = new Chicken();
+        Chicken chicken2 = new Chicken();
+        Cow cow = new Cow();
+        
+        System.out.print("The zombie attacked the cow and was ");zombie.interact(cow);
+        
+        System.out.print("The cow then died and dropped raw ");cow.food();
+        
+        System.out.print("The 2 chickens bred and now ");chicken1.breed(chicken2);
+        
+        System.out.print("The zombie attempted to interact with the creeper but was ");zombie.interact(creeper);
+        
+        System.out.print("The creeper then was ");creeper.interact(zombie);System.out.print(" and blew up the zombie");
+        
     }
     
 }
